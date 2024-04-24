7b82e14e79dfdaafcdf536405c542d96d182220a
syed farooq <syed@saafe.in>
2024-04-24 21:38:00 +0530

    filter using useState ep5

--- src/Component/Body.js
+++ src/Component/Body.js
@@ -1,18 +1,33 @@
 import Restaurent from "./Restaurent";
 import resList from "../Utils/mockData";
+import React, { useState } from "react";
 
-const Body = () => (
-  <div className="body">
-    <div className="search">
-      <input className="search-input"></input>
-      <button className="search-button">Search</button>
+const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  return (
+    <div className="body">
+      <div className="search">
+        <input className="search-input"></input>
+        <button className="search-button">Search</button>
+      </div>
+      <button
+        className="rating-filter-btn"
+        onClick={() => {
+          const filteredData = listOfRestaurants.filter(
+            (res) => res.info.avgRatingString > 4
+          );
+          setListOfRestaurants(filteredData);
+        }}
+      >
+        TOP Rated Restaurent
+      </button>
+      <div className="restaurent-container">
+        {listOfRestaurants.map((restaurent) => (
+          <Restaurent key={restaurent.info.id} resData={restaurent} />
+        ))}
+      </div>
     </div>
-    <div className="restaurent-container">
-      {resList.map((restaurent) => (
-        <Restaurent key={restaurent.info.id} resData={restaurent} />
-      ))}
-    </div>
-  </div>
-);
+  );
+};
 
 export default Body;
--- src/Utils/constants.tsx
+++ src/Utils/constants.tsx
@@ -1,7 +1,9 @@
-export const LOGO_URL =
+ const LOGO_URL =
   "https://graphicsfamily.com/wp-content/uploads/edd/2021/06/Editable-Photoshop-Food-Logo-Design-PNG-Transparent.png";
 
-export const CDN_URL =
+ const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-export const CART_URL =
+ const CART_URL =
   "https://t3.ftcdn.net/jpg/03/14/84/68/360_F_314846831_5jJsC7Us9obgwMjRDqFhs04dodzvnZvi.jpg";
+
+export { LOGO_URL,CDN_URL,CART_URL };
